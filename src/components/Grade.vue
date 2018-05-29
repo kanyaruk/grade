5227d2d58c9dca20f4f7f55f7331fa58a82f42c3
kanyaruk <ahmkanyaruk@hotmail.com>
2018-05-29 10:15:43 +0700

    add student

--- src/components/Grade.vue
+++ src/components/Grade.vue
@@ -1,6 +1,26 @@
 <template>
-  <div class="hello">
+  <div class="hello container">
     Grade
+    <div class="columns">
+      <div class="column">
+        <b-field label="รหัสนักศึกษา">
+          <b-input v-model="sid"></b-input>
+        </b-field>
+      </div>
+      <div class="column">
+        <b-field label="ชื่อ-สกุล">
+          <b-input v-model="name"></b-input>
+        </b-field>
+      </div>
+    </div>
+    <div class="columns column is-centered">
+      <button class="button is-success" @click="addStudent(sid, name)">ADD</button>
+    </div>
+    <div class="columns">
+      <ui :key="student.sid" v-for="student in students">
+        <li>{{student.sid}} {{student.name}}</li>
+      </ui>
+    </div>
   </div>
 </template>
 
@@ -10,31 +30,35 @@ var config = {
   databaseURL: 'https://grade-6b43d.firebaseio.com'
 }
 firebase.initializeApp(config)
-
+var db = firebase.database()
+var gradeRef = db.ref('/grade')
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      students: {},
+      sid: '5806021631017',
+      name: 'kanyaruk'
     }
+  },
+  methods: {
+    addStudent (sid, name) {
+      gradeRef.child(sid).set({
+        sid,
+        name
+      })
+      this.sid = ''
+      this.name = ''
+    }
+  },
+  created () {
+    gradeRef.on('value', snap => {
+      this.students = snap.val()
+    })
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
